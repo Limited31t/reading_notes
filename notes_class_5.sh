eac7730fc7d663c2d41da9efd17452a0c805369c
Gary King <109040766+Limited31t@users.noreply.github.com>
2022-08-01 17:37:32 -0700

    Create notes_class_5.sh

---
+++ notes_class_5.sh
@@ -0,0 +1,16 @@
+Gary King
+07/29/2022
+Class 05 reading notes
+					SMBP Protocol
+SMBP  is Server Message Block Protocol where a client and server communicate and have access to files, printer & serial ports on a network.
+
+The connection is being allowed to access into the system where it can make changes to the server as in repairs or even any necessary updates can be installed.
+
+These connections are made via an OPEN port that is usually port 139 and or 445. Port 139 will use connector NETBIOS and the 445 will connect on top of a TCP stack.
+These ports however can be dangerous if they are not set up properly because they are exposed to the public because when they are OPEN they leave the server vulnerable to exploits.
+
+The most dangerous ports are known as WORMABLE ports where it hits your computer and runs over the unit because it does not need to be commanded to open and moves freely on its own to the whole unit.
+
+A computer that has been infected will start searching the device for more files to corrupt.
+Microsoft is still continuing to try to improve the SMB performance and security.
+
