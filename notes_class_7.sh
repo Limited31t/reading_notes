44573f1738e1de404c98141b7c08dd9eb4e70442
Gary King <109040766+Limited31t@users.noreply.github.com>
2022-08-02 08:02:14 -0700

    Create notes_class_7.sh

---
+++ notes_class_7.sh
@@ -0,0 +1,19 @@
+Gary King
+08/02/2022
+Class 06 reading notes
+PowerShell
+A Power Shell is a command line shell interface that Microsoft made and it enables system administrators to manage computers from the command line that has a scripting language that is built within itself.
+Power shell is an interpreted language compiled for doing things to manage a technology environment. 
+SOme of the people that would use PowerShell would be a developer, a web developer or like a software developer and some other job titles would be SystemsAdinistrator and Systems Engineer.
+Some think that PowerShells are less cumbersome than using BASH because Bash requires a lot of string manipulations whereas PowerShell does not.
+PowerShell is the main choice for Windows machines and Bash is mainly used with Linux machines but both are becoming better at managing on both machines.
+Some of the reasons one would want to use a PowerShell is that it could make automation because you could work faster, its accuracy to help reduce your mistakes, the versatility to where it can be utilized, the PowerShell community could help make you a better administrator and last PowerShellcontinues to grow and adapt.
+
+HAckers are using PowerShell to get into system without leaving much of a footprint instead using installed files.
+Administrators in the past only had to worry about Maleware being as an executable file but now have to worry about the hackers getting into their systems with some type of script.
+
+Hackers are running in the memory using PowerShell scripting language to steal passwords and they are also wrapping protections around PowerShell to keep it from being abused .
+
+PowerShell is widely used for data collections but hackers use it to get into the file system and place malicious codes straight into the memory, they also get access from databases and buckets are left open to the public and they can get in via these areas.
+
+Phishing was found to be about 29% of the attacks.
