6e238436825cc911e6c3c75f006d99750289bc27
Gary King <109040766+Limited31t@users.noreply.github.com>
2022-08-01 17:44:11 -0700

    Create notes_class_3.sh

---
+++ notes_class_3.sh
@@ -0,0 +1,13 @@
+Gary King
+07/27/2022
+Class 03 reading notes
+The Role of Help Desk
+The help desk is there to help customers that are having some type of computer software problems.
+A customer will put in a help ticket and an IT /staff member will receive the ticket and proceed to help by asking open ended questions like does the unit turn and that is opened so the customer answers yes or no that then leads into another question till hopefully they can find some kind of issue and get it resolved.
+If there are 2 tickets that come in and one is from a customer that has a SERVER issue, they will more than likely take that one first due to the server has multiple employees that work on it and they will be able to be working as normal VS a home type computer that doesn't really have a business attached to it.
+
+There are 2 skill sets involved in an IT person that employers will look for when hiring. There are the HARD SKILLs where the employee will have technical skills where they can configure,do maintenance and also diagnose  machines as well as having a business to get people back on the computers.
+
+Then there are the SOFT Skills, one that has great communication skills where they can speak with customers and get the answers that they need to help fix the machines. They also have good writing skills so they can explain the problem in written form so we can all understand not just an IT type person.
+
+Critical thinking is a good skill to have because they are thinking of the issue, then asking specific questions.  He also needs to be able to bring in different perspectives while diagnosing a problem.
