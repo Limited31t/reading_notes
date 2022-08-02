ad6f6966129388e5ed970d620f41223ac812b7ff
Gary King <109040766+Limited31t@users.noreply.github.com>
2022-08-01 17:42:03 -0700

    Create notes_class_4.sh

---
+++ notes_class_4.sh
@@ -0,0 +1,19 @@
+Gary King
+
+07/28/2022
+
+Class 05 reading notes
+
+Troubleshooting Techniques
+
+ 
+
+An IT person's job is to identify or troubleshoot a problem on a computer.
+
+IT has to listen to the customer about the issue because since they have been dealing with the problem they can usually let IT know what usually happens at the beginning of the issue thru to the end of the issue and this will help IT solve the issue a little quicker. This is when IT will need to be asking the open ended questions so that they find a solution.
+
+If IT finds that there are multiple issues going on then they would need to break the issues down one at a time and solve them that way.
+
+They will usually make a list of potential things that could be wrong and then have a solution for that problem and then start running those checks to eliminate those problems.
+
+Once you have made a change to the system for the repair you then need to test the computer and see if it works properly and when it does bring in the customer to let them try to run it and see if it works for them. If that does not fix the problem then continue down your list of possibilities until the problem is solved.
